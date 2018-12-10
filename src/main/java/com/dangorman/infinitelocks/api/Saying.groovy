7fdd178b9638efc5d5e92345000e612b7493e7cd
Dan Gorman <dgorman@commercehub.com>
2018-12-10 16:59:53 -0500

    added gitignore

---
+++ src/main/java/com/dangorman/infinitelocks/api/Saying.groovy
@@ -0,0 +1,27 @@
+package com.dangorman.infinitelocks.api
+
+class Saying {
+    private long id;
+
+    @Length(max = 3)
+    private String content;
+
+    public Saying() {
+        // Jackson deserialization
+    }
+
+    public Saying(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+}
