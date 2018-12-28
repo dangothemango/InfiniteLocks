3f5bbe13f310386a156ae09f79bb5eff879e6426
Dan Gorman <dgorman@commercehub.com>
2018-12-28 17:31:43 -0500

    Made the locks endpoint read from the database

--- src/main/java/com/dangorman/infinitelocks/resources/LockResource.java
+++ src/main/java/com/dangorman/infinitelocks/resources/LockResource.java
@@ -1,14 +1,16 @@
 package com.dangorman.infinitelocks.resources;
 
+import com.dangorman.infinitelocks.db.DatabaseModule;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
-import org.jtwig.web.servlet.JtwigRenderer;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.xml.ws.http.HTTPException;
+import java.sql.SQLException;
 
 
 @Path("/lock")
@@ -17,7 +19,19 @@ public class LockResource {
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String getLock(@QueryParam("lock") String lock) {
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("assets/Puzzles/Colors.html");
+        String lockLocation;
+        if (lock.contains(";")){
+            throw new HTTPException(401);
+        }
+        try {
+            lockLocation = (String)DatabaseModule.getDbConnection().rows(String.format("Select location from locks where name = '%s' limit 1")).get(0).get("location");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            //TODO make all 404s better ---idea Secret locks
+            throw new HTTPException(404);
+        }
+
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(lockLocation);
 
         return template.render(new JtwigModel());
     }
